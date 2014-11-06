fbfd0e738b0214958f62a49aedd55d2bfeb25a15
Naner Nunes <naner@naner.com.br>
2014-11-05 23:49:23 -0200

    Crontab file working and accepting requests

---
+++ crontab.php
@@ -0,0 +1,48 @@
+<?php
+
+	$dir = dirname(__FILE__) . "/../../";
+	include( "{$dir}/lib/config.php" );
+	include( "{$dir}/data/data_instances.php" );
+
+	class YoutubeResponse {
+
+		function __construct( $channel ) {
+			$this->channel = $channel;
+		}
+
+		function getVideo() {
+
+			$doc = new DOMDocument();
+			$url = "https://gdata.youtube.com/feeds/api/users";
+			$url .= "/{$this->channel}/uploads?max-results=1";
+
+			@$doc->loadHTMLFile($url);
+			$xpath = new DOMXpath($doc);
+
+			foreach ($xpath->query("//id[contains(text(),'videos')]") as $node) {
+				$video = preg_replace('/.*\/(.+)$/', '$1', $node->nodeValue);
+			}
+
+			return $video;
+		}
+
+	}
+
+
+	$hooks = array_filter($data, function($entry) {
+		return ($entry['plugin'] == 'youtube_subscriptions');
+	});
+
+	foreach ($hooks as $id => $hook) {
+
+		$youtube = new YoutubeResponse($hook['youtube']);
+
+		$options = array('http' => array('method'  => 'POST',
+			'content' => http_build_query(array('video' => $youtube->getVideo() )) ));
+
+		file_get_contents("{$cfg['root_url']}hook.php?id={$id}&token={$hook['token']}",
+			false, stream_context_create($options) );
+
+	}
+
+?>
